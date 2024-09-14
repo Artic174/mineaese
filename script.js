0c7eaa942c1caa91e0c2453d0886b5767b411429
Artic174 <142976550+Artic174@users.noreply.github.com>
2024-09-14 19:09:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-document.querySelector('button').addEventListener('click', function() {
-    alert('Terima kasih telah bergabung!');
+document.querySelectorAll('nav a').forEach(item => {
+    item.addEventListener('mouseover', event => {
+        item.style.color = '#ff5722'; // Mengubah warna saat hover
+    });
+    item.addEventListener('mouseout', event => {
+        item.style.color = '#4a4a4a'; // Mengembalikan warna saat tidak hover
+    });
 });
- 
